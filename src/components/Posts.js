ab33afcafe6b44d0c9314416c3ec4943cf13f106
Siddheshwar Panda <siddheshwar.2307@gmail.com>
2021-10-26 13:09:22 +0530

    get posts redux complete

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 // redux imports
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions/postActions";
 
@@ -24,6 +25,11 @@ class Posts extends Component {
     );
   }
 }
+
+Posts.protoTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   posts: state.posts.items,
